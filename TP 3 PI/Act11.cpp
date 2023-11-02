95bafd40259c405101e3b60454aed6159ed170a5
lautaroalvarez11 <infolautaroalvarez@gmail.com>
2023-11-01 21:34:53 -0300

    .

--- TP 3 PI/Act11.cpp
+++ TP 3 PI/Act11.cpp
@@ -1,3 +1,10 @@
+/*
+Implementar una función que reciba dos listas enlazadas simples con datos de tipo string ordenadas según el
+alfabeto y retorne una tercera lista ordenada por el mismo criterio, conformada por todos los elementos de las
+listas originales (merge).
+Agregar las funciones auxiliares que sean necesarias para cumplir con este propósito.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
--- TP 3 PI/Act12.cpp
+++ TP 3 PI/Act12.cpp
@@ -1,3 +1,15 @@
+/*
+Generar una lista enlazada simple con la nómina de artículos de un supermercado. De cada artículo se conoce:
+código, descripción, precio y stock. La carga finaliza con el código -1. Se solicita:
+
+A. Leer de teclado un porcentaje e incrementar el precio de todos los artículos en esa cantidad.
+
+B. Incrementar el stock de un artículo. Se lee de teclado el código y la cantidad con la que se debe
+incrementar el stock. Si no existe el artículo en la lista se debe informar.
+
+C. Eliminar de la lista los artículos que no tienen stock.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
