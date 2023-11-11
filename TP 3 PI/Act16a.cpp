c7ae0e54ee7624f50590b4f8ce4b595d06a10b9c
lautaroalvarez11 <infolautaroalvarez@gmail.com>
2023-11-11 01:24:01 -0300

    .

---
+++ TP 3 PI/Act16a.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Nodo
+{
+    int dato;
+    Nodo* siguiente;
+};
+
+Nodo* insertarAlPrincipio(Nodo* fin, int numero)
+{
+    Nodo* nuevoNodo = new Nodo; // Se crea un nodo para almacenar el valor
+    nuevoNodo -> dato = numero; // El dato que va a contener el nuevo nodo es el que ingrese el usuario
+
+    if (fin == nullptr) // Si la lista esta vacía
+    {
+      nuevoNodo -> siguiente = nuevoNodo; // El nuevo nodo va a apuntarse a sí mismo, es decir va a ser el primer elemento
+      fin = nuevoNodo; // El final es el inicio, es decir el nuevo nodo va a ser el primero y el ultimo
+    }
+    else // Si la lista no está vacía
+    {
+      nuevoNodo -> siguiente = fin -> siguiente; // El nuevo nodo va a apuntar al nodo que apunte el puntero fin
+      fin -> siguiente = nuevoNodo; // Y el puntero fin va a apuntar al nuevo nodo porque es el primer elemento
+    }
+
+    fin = nuevoNodo; // Actualizar el puntero fin para que apunte al nuevo nodo
+
+    return fin; // Retorna el puntero fin porque apunta al primer elemento de la lista
+}
+
+void mostrarLista(Nodo* fin)
+{
+    cout << "Lista: ";
+    for(Nodo* temporal = fin; temporal != nullptr; temporal = temporal -> siguiente)
+    {
+        cout << temporal -> dato << " ";
+    }
+}
+
+
+
+int main()
+{
+    Nodo* fin = new Nodo;
+    fin = nullptr; // Declarar lista vacía
+
+    int numero;
+
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+
+    while(numero != 0)
+    {
+        fin = insertarAlPrincipio(fin, numero);
+        cout << "Ingrese otro numero: ";
+        cin >> numero;
+    }
+    
+    mostrarLista(fin);
+
+    return 0;
+}
---
+++ TP 3 PI/Act16b.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Nodo
+{
+    int dato;
+    Nodo* siguiente;
+};
+
+Nodo* insertarAlFinal(Nodo* fin, int numero)
+{
+    Nodo* nuevoNodo = new Nodo; // Se crea un nodo para almacenar el valor
+    nuevoNodo -> dato = numero; // El dato que va a contener el nuevo nodo es el que ingrese el usuario
+
+    if (fin == nullptr) // Si la lista está vacía
+    {
+        nuevoNodo -> siguiente = nuevoNodo; // El nuevo nodo debe apuntarse a sí mismo
+    }
+    else // Si la lista no está vacía
+    {
+        nuevoNodo -> siguiente = fin -> siguiente; // El nuevo nodo va a apuntar al nodo que apunte el puntero fin
+        fin -> siguiente = nuevoNodo; // // El puntero fin va a apuntar al nuevo nodo porque es el primer elemento
+    }
+    return nuevoNodo; // Retorna el ultimo numero insertado en la lista
+
+}
+
+void mostrarLista(Nodo* fin)
+{
+    cout << "Lista: ";
+    for(Nodo* temporal = fin; temporal != nullptr; temporal = temporal -> siguiente)
+    {
+        cout << temporal -> dato << " ";
+    }
+}
+
+
+
+int main()
+{
+    Nodo* fin = new Nodo;
+    fin = nullptr; // Declarar lista vacía
+
+    int numero;
+
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+
+    while(numero != 0)
+    {
+        fin = insertarAlFinal(fin, numero);
+        cout << "Ingrese otro numero: ";
+        cin >> numero;
+    }
+    
+    mostrarLista(fin);
+
+    return 0;
+}
