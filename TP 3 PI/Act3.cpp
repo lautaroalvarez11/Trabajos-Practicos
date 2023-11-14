3f7d9d8627ea3a62c1337858b188dfe4d4bbeee5
lautaroalvarez11 <infolautaroalvarez@gmail.com>
2023-11-14 20:06:47 -0300

    .

--- TP 3 PI/Act3.cpp
+++ TP 3 PI/Act3.cpp
@@ -4,17 +4,17 @@ using namespace std;
 
 int main()
 {
-    char *x = new char;
-    char *z = x;
+    char *x = new char; // La variable del puntero x se declara en la memoria heap
+    char *z = x; // El puntero z tiene la misma dirección de memoria que el puntero x
 
     //A
-    *x = 'M';
+    *x = 'M'; // Al puntero x se le asigna la letra M
 
     //B
-    cout << *x;
+    cout << *x; // M
 
     //C
-    *z = 'P';
+    *z = 'P'; // Al puntero z se le asigna la letra P
 
     //D
     cout << *z << " " << *x;
@@ -31,16 +31,16 @@ int main()
     */
 
    //F
-   x = new char;
+   x = new char; // Se asigna a la variable x en la memoria heap
 
    //G
-   cin >> *x;
+   cin >> *x; // Se almacena en la dirección de memoria de la variable x
 
    //H
-   if (*x == '*')
+   if (*x == '*') // Si el valor de la dirección de memoria de la variable x es igual a *
    {
         cout << "Asterisco";
    }
-
+   
     return 0;
 }
